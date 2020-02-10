591e03a83cdef488c28295133833adcb14c13db4
congliqiang <2785347264@qq.com>
2020-02-10 11:56:24 +0800

    注册与登录

--- app/Providers/RiakServiceProvider.php
+++ app/Providers/RiakServiceProvider.php
@@ -15,9 +15,6 @@ class RiakServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->singleton(Connection::class, function ($app) {
-            return new Connection(config('riak'));
-        });
     }
 
     /**
